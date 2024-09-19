c2a545c37dfa2fbfdbe7f01d033778f4f4e6c877
KaSaNaa <pasindudeshitha123@gmail.com>
2024-09-19 23:33:40 +0530

    Add dark and light themes to the project

---
+++ src/theme/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
+
+export { darkTheme, lightTheme };
