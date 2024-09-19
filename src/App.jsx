2ab068b75337df6d4bfb534de999ecd84b9cfaa8
KaSaNaa <pasindudeshitha123@gmail.com>
2024-09-19 23:32:10 +0530

    Refactor App.jsx to add dark theme and private routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,26 @@ import HomePage from "./components/home/HomePage";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
 import SignOut from "./components/auth/SignOut";
-import { handleGoogleSignIn, handleGithubSignIn, handleEmailSignIn } from "./services/authService";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+// eslint-disable-next-line no-unused-vars
+import {darkTheme, lightTheme} from "./theme/theme";
+import PrivateRoute from "./components/auth/PrivateRoute";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/post-something" element={<PostPage />} />
-        <Route path="/find-questions" element={<FindQuestionPage />} />
-        <Route path="/signin" element={<SignIn handleEmailSignIn={handleEmailSignIn} handleGoogleSignIn={handleGoogleSignIn} handleGithubSignIn={handleGithubSignIn} />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signout" element={<SignOut />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/post-something" element={<PrivateRoute><PostPage /></PrivateRoute>} />
+          <Route path="/find-questions" element={<PrivateRoute><FindQuestionPage /></PrivateRoute>} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signout" element={<SignOut />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
