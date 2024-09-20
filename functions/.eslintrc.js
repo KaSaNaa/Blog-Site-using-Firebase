0f175d7622551c64ab83c883f6b70cba45d6f005
KaSaNaa <pasindudeshitha123@gmail.com>
2024-09-20 19:25:28 +0530

    Add Firebase configuration files and ESLint setup

---
+++ functions/.eslintrc.js
@@ -0,0 +1,29 @@
+export default {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    "ecmaVersion": 2018,
+    "sourceType": "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
