368aac32aa3401444e5eb81893ffde10d4301777
KaSaNaa <pasindudeshitha123@gmail.com>
2024-09-20 19:25:48 +0530

    Refactor subscribe form to use HTTPS for API endpoint and JSON for request body

--- src/components/home/subsEmail.jsx
+++ src/components/home/subsEmail.jsx
@@ -15,12 +15,12 @@ export default function SubscribeForm() {
     event.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3000/subscribeAction", {
+      const response = await fetch("https://us-central1-devdeakinlogin.cloudfunctions.net/sendWelcomeEmail", {
         method: "POST",
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
+          "Content-Type": "application/json",
         },
-        body: new URLSearchParams({ email }),
+        body: JSON.stringify({ email }),
       });
 
       if (response.ok) {
@@ -35,50 +35,50 @@ export default function SubscribeForm() {
   };
 
   return (
-      <Container>
-        <Divider style={{ margin: "10px 0" }} />
-        <Container
+    <Container>
+      <Divider style={{ margin: "10px 0" }} />
+      <Container
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{ flexGrow: 1, textAlign: "center", margin: "20px" }}
+        >
+          Subscribe to our Newsletter
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
           sx={{
-            width: "100%",
             display: "flex",
             flexDirection: "row",
-            justifyContent: "space-between",
+            justifyContent: "center",
             alignItems: "center",
+            gap: 2,
           }}
         >
-          <Typography
-            variant="h5"
-            sx={{ flexGrow: 1, textAlign: "center", margin: "20px" }}
-          >
-            Subscribe to our Newsletter
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              gap: 2,
-            }}
-          >
-            <TextField
-              required
-              id="email"
-              name="email"
-              label="Email"
-              type="email"
-              margin="normal"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Button type="submit" variant="contained" color="primary">
-              Subscribe
-            </Button>
-          </Box>
-        </Container>
-        <Divider style={{ margin: "10px 0" }} />
+          <TextField
+            required
+            id="email"
+            name="email"
+            label="Email"
+            type="email"
+            margin="normal"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Subscribe
+          </Button>
+        </Box>
       </Container>
+      <Divider style={{ margin: "10px 0" }} />
+    </Container>
   );
 }
