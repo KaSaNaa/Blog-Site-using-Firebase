6adfe68d875f9097bdf959cdc17720d467e0e4f1
KaSaNaa <pasindudeshitha123@gmail.com>
2024-09-24 11:04:32 +0530

    Refactor HeaderImg component to adjust height, hide overflow, and add background image

--- src/components/home/HeaderImg.jsx
+++ src/components/home/HeaderImg.jsx
@@ -1,6 +1,5 @@
 import Card from "@mui/material/Card";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import LogoSvg from "../../assets/logo.svg";
 
 const darkTheme = createTheme({
   palette: {
@@ -8,19 +7,37 @@ const darkTheme = createTheme({
   },
 });
 
+const gifUrl =
+  "https://cdn.dribbble.com/users/244516/screenshots/15180098/media/35301f78c0ba0d242a6703b927814e23.gif"; // Replace with your GIF URL
+
 export default function HeaderImg() {
   return (
     <ThemeProvider theme={darkTheme}>
       <Card
         sx={{
           maxWidth: "100%",
-          height: "350px",
+          height: { xs: "190px", sm: "285px", md: "332.5px" }, // Adjusted height to account for cropping
           margin: "20px",
           padding: "80px",
-          backgroundSize: "50%",
-          background: `url(${LogoSvg}) no-repeat center/cover`,
+          overflow: "hidden", // Hide overflow
+          position: "relative",
+          backgroundColor: "white"
         }}
-      />
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: "-5%", // Move the bottom up by 5%
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundImage: `url(${gifUrl})`,
+          }}
+        />
+      </Card>
     </ThemeProvider>
   );
 }
--- src/services/users/index.js
+++
@@ -1,8 +0,0 @@
-import { getAuth } from "firebase/auth";
-
-const auth = getAuth();
-
-export const getUserDisplayName = async (uid) => {
-  const user = await auth.get(uid);
-  return user.displayName;
-};
