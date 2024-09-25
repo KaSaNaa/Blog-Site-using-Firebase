fd681800d421eb33e9abb159850a726001e62f9a
KaSaNaa <pasindudeshitha123@gmail.com>
2024-09-25 21:35:45 +0530

    Refactor: Use custom theme provider in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {CssBaseline, ThemeProvider} from "@mui/material";
-// eslint-disable-next-line no-unused-vars
-import {darkTheme, greenTheme, lightTheme, redTheme} from "./theme/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 import PrivateRoute from "./components/auth/PrivateRoute";
 import { lazy, Suspense } from "react";
 import Spinner from "./components/misc/Spinner";
 import ArticleView from "./components/posts/ArticleView";
+import { ThemeProvider as CustomThemeProvider } from "./contexts/ThemeContext"; // Fix typo
+import useThemeContext from "./hooks/useTheme";
 
 const SignUp = lazy(() => import("./components/auth/SignUp"));
 const PostPage = lazy(() => import("./components/posts/Postpage"));
@@ -15,28 +16,31 @@ const SignIn = lazy(() => import("./components/auth/SignIn"));
 const SignOut = lazy(() => import("./components/auth/SignOut"));
 
 function App() {
+  const { theme } = useThemeContext();
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-      <Suspense fallback={
-        <Spinner color="#4fa94d" text="Dev@Deakin is loading..." />
-      }>
-        <Routes>
-          <Route path="/post-something" element={<PrivateRoute><PostPage /></PrivateRoute>} />
-          <Route path="/find-questions" element={<PrivateRoute><FindQuestionPage /></PrivateRoute>} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signout" element={<SignOut />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/article/:articleId" element={<ArticleView />} />
-        </Routes>
+        <Suspense fallback={<Spinner color="#4fa94d" text="Dev@Deakin is loading..." />}>
+          <Routes>
+            <Route path="/post-something" element={<PrivateRoute><PostPage /></PrivateRoute>} />
+            <Route path="/find-questions" element={<PrivateRoute><FindQuestionPage /></PrivateRoute>} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signout" element={<SignOut />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/article/:articleId" element={<ArticleView />} />
+          </Routes>
         </Suspense>
       </Router>
     </ThemeProvider>
   );
 }
 
-export default App;
-
-// TODO: Make the chatbot
+export default function AppWrapper() {
+  return (
+    <CustomThemeProvider> {/* Fix typo */}
+      <App />
+    </CustomThemeProvider>
+  );
+}
