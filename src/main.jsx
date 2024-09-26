dd6078c83b9934a017660ff459b50216a6ec505c
KaSaNaa <pasindudeshitha123@gmail.com>
2024-09-26 20:47:30 +0530

    Refactor: Update component import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext";
+import AppWrapper from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <AppWrapper />
     </AuthProvider>
   </StrictMode>
 );
