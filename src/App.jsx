d2f201a7465504eead987d7a3346b6a2edbb7b53
KaSaNaa <pasindudeshitha123@gmail.com>
2024-09-26 20:47:24 +0530

    Refactor: Add framer-motion package and implement page transitions in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { AnimatePresence, motion } from "framer-motion";
 
 import PrivateRoute from "./components/auth/PrivateRoute";
 import { lazy, Suspense } from "react";
@@ -15,24 +16,33 @@ const HomePage = lazy(() => import("./components/home/HomePage"));
 const SignIn = lazy(() => import("./components/auth/SignIn"));
 const SignOut = lazy(() => import("./components/auth/SignOut"));
 
+const pageTransition = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -20 },
+  transition: { duration: 0.5 }
+};
+
 function App() {
   const { theme } = useThemeContext();
+  const location = useLocation();
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
+      <AnimatePresence mode="wait">
         <Suspense fallback={<Spinner color="#4fa94d" text="Dev@Deakin is loading..." />}>
-          <Routes>
-            <Route path="/post-something" element={<PrivateRoute><PostPage /></PrivateRoute>} />
-            <Route path="/find-questions" element={<PrivateRoute><FindQuestionPage /></PrivateRoute>} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signout" element={<SignOut />} />
-            <Route path="/" element={<HomePage />} />
-            <Route path="/article/:articleId" element={<ArticleView />} />
+          <Routes location={location} key={location.pathname}>
+            <Route path="/post-something" element={<motion.div {...pageTransition}><PrivateRoute><PostPage /></PrivateRoute></motion.div>} />
+            <Route path="/find-questions" element={<motion.div {...pageTransition}><PrivateRoute><FindQuestionPage /></PrivateRoute></motion.div>} />
+            <Route path="/signin" element={<motion.div {...pageTransition}><SignIn /></motion.div>} />
+            <Route path="/signup" element={<motion.div {...pageTransition}><SignUp /></motion.div>} />
+            <Route path="/signout" element={<motion.div {...pageTransition}><SignOut /></motion.div>} />
+            <Route path="/" element={<motion.div {...pageTransition}><HomePage /></motion.div>} />
+            <Route path="/article/:articleId" element={<motion.div {...pageTransition}><ArticleView /></motion.div>} />
           </Routes>
         </Suspense>
-      </Router>
+      </AnimatePresence>
     </ThemeProvider>
   );
 }
@@ -40,7 +50,9 @@ function App() {
 export default function AppWrapper() {
   return (
     <CustomThemeProvider> {/* Fix typo */}
-      <App />
+      <Router>
+        <App />
+      </Router>
     </CustomThemeProvider>
   );
 }
